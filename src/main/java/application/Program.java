20465dae1802b74201fdd6f0b17ff5cc6f23b5b2
Nicoly Ott <nicolyott23@gmail.com>
2024-11-04 10:41:53 -0300

    update implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -17,7 +17,6 @@ public class Program {
         System.out.println(seller);
 
         System.out.println(" ");
-
         System.out.println("=== TESTE 2: seller findByDepartment ===");
         Department dep = new Department(2,null);
         List<Seller> list = sellerDao.findByDepartment(dep);
@@ -26,19 +25,26 @@ public class Program {
         }
 
         System.out.println(" ");
-
         System.out.println("=== TESTE 3: seller findByDepartment ===");
         list = sellerDao.findAll();
         for (Seller obj : list){
             System.out.println(obj);
         }
 
-        System.out.println(" ");
-
+//        System.out.println(" ");
 //        System.out.println("=== TESTE 4: seller insert ===");
 //        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 //        sellerDao.insert(newSeller);
 //        System.out.println("Inserted! New id = " + newSeller.getId());
+
+//        System.out.println(" ");
+//        System.out.println("=== TESTE 5: seller findByDepartment ===");
+//        seller = sellerDao.findById(1);
+//        seller.setName("Martha Waine");
+//        sellerDao.update(seller);
+//        System.out.println("Update completed");
+
+        
 
     }
 }
